c1fe58981335cbae7347125af69f31f2ba54c0a4
Pavel Zyryanov <zyryanov@outlook.lv>
2024-06-04 20:27:54 +0300

    revert types.go changes

--- db/types.go
+++ db/types.go
@@ -15,13 +15,11 @@ type Article struct {
 type ErrorMap map[string][]error
 
 func (a Article) Validate() ErrorMap {
-	var errs []error
+	errs := make(ErrorMap)
 
 	if a.Title == "" {
 		err := errors.New("title cant be empty")
 		errs["title"] = append(errs["title"], err)
-
-		errs = append(errs, err)
 	}
 	if a.Slug == "" {
 		err := errors.New("slug cant be empty")
